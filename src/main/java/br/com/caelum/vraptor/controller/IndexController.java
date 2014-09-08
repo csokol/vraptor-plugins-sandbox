13a24d35541a392f50c7bfd5c23376edfe77efa3
Francisco Sokol <chico.sokol@gmail.com>
2014-09-08 18:55:11 -0300

    removing vraptor-form stuff

--- src/main/java/br/com/caelum/vraptor/controller/IndexController.java
+++ src/main/java/br/com/caelum/vraptor/controller/IndexController.java
@@ -11,17 +11,12 @@ public class IndexController {
 	@Inject
 	private Result result;
 
-	@Inject
-	private ModelForm<User> modelForm;
-
 	@Get("/")
 	public void index() {
 		result.include("variable", "VRaptor!");
-		result.include("form", modelForm);
 	}
 	@Post("/")
 	public void index(User user) {
 		result.include("user", user);
-		result.include("form", modelForm);
 	}
 }
